af26f5556a0159ccf7bec8985e3b5345b88ac7ec
yinzhijiang <1500059351@qq.com>
2018-05-10 10:16:39 +0800

    change

--- vueBlog/config/index.js
+++ vueBlog/config/index.js
@@ -53,12 +53,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/jyz/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist/jyz/'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/jyz/',
 
     /**
      * Source Maps
